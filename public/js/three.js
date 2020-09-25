570403b8145dc000a3cd4f9544ef4ddf71f036cb
JeanneBarriere <barriere.jeanne@gmail.com>
2020-09-25 15:45:34 +0200

    test

--- public/js/three.js
+++ public/js/three.js
@@ -6,20 +6,49 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
 camera.position.z = 2;
+camera.position.y = 2;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x6de6ff, 1);
 document.body.appendChild(renderer.domElement);
+
+const planeSize = 10;
+
+const loader = new THREE.TextureLoader();
+const texture = loader.load('../images/grass.jpg');
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.magFilter = THREE.NearestFilter;
+const repeats = planeSize / 4;
+texture.repeat.set(repeats, repeats);
+
+const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+const planeMat = new THREE.MeshPhongMaterial({
+  map: texture,
+  side: THREE.DoubleSide,
+});
+const mesh = new THREE.Mesh(planeGeo, planeMat);
+mesh.rotation.x = Math.PI * -.5;
+scene.add(mesh);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true
-});
+const material = new THREE.MeshLambertMaterial({color: 0xEAEFF2});
+
 const cube = new THREE.Mesh(geometry, material);
+cube.position.y = 0.5;
 scene.add(cube);
+
+const color = 0xFFFFFF;
+const intensity = 0.5;
+const light = new THREE.AmbientLight(color, intensity);
+scene.add(light);
+
+var point_light = new THREE.PointLight(0xFFFFFF);
+point_light.position.set(10, 15, 50);
+scene.add(point_light);
 
 window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -37,7 +66,7 @@ var animate = function () {
     // cube.rotation.y += 0.01;
     controls.update();
     render();
-    stats.update();
+    //stats.update();
 };
 
 function render() {
